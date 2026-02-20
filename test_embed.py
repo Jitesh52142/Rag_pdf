c5e672550b2d736604290ae3af74a96af9dfe026
Jitesh Bawaskar <jitesh.bawaskar@softude.com>
2026-02-20 13:00:18 +0530

    final commit

--- test_embed.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-import os
-
-client = genai.Client(api_key="AIzaSyBzaaE0DVrwJkSOU11K4cEu-AeiZbfufF0")
-
-models = client.models.list()
-
-for m in models:
-    print(m.name)
--- test_supabase.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-url = "https://epvgkepgluchuchwrihi.supabase.co"
-print(requests.get(url).status_code)
